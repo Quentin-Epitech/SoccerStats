ed6538551d566e701a01afe59a79430abaac8930
Quentin-Epitech <quentin.bonnet@epitech.digital>
2025-09-30 10:57:35 +0200

    csv

--- Clean.py
+++ Clean.py
@@ -6,4 +6,10 @@ print(read.shape)
 print(read.isna().sum())
 
 
+nettoyage = read.fillna("None")
+
+output_path = "Clean.csv"
+nettoyage.to_csv(output_path, index=False)
+
+print(f"Fichier: {output_path}")
 
